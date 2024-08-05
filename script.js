50cdc6f85ac5873d828d1a672ce0e3ee85b86785
Code Malayalam <anusreemanojkumark@gmail.com>
2024-08-05 23:22:05 +0530

    flipkart second commit

--- script.js
+++ script.js
@@ -32,23 +32,47 @@ document.addEventListener('DOMContentLoaded', () => {
         headerContent.appendChild(cartContent)
 
       });
-      const nav=document.querySelector(".nav-padding")
-      data.navBar.forEach(item=>{
-        const navItem=document.createElement("div")
-        navItem.className="nav-item"
-        navItem.innerHTML=`<span>${item.navText}</span><img src="${item.navArrow}">`
+      const nav = document.querySelector(".nav-padding")
+      data.navBar.forEach(item => {
+        const navItem = document.createElement("div")
+        navItem.className = "nav-item"
+        navItem.innerHTML = `<span>${item.navText}</span><img src="${item.navArrow}">`
         nav.appendChild(navItem)
       })
-      const main=document.querySelector(".mainTop-padding")
-      const topMain=document.createElement("div")
-      topMain.className="top-content"
-      main.appendChild(topMain)
-      data.main.mian-header.header-top.forEach(item=>{
-        const topItem=document.createElement("div")
-         topItem.className="top-item"
-         
-
+      const sidBar = document.querySelector(".sidebar")
+      const sideBarTop = document.createElement("div")
+      sideBarTop.className = "filter"
+      sideBarTop.innerHTML = `<span>${data.sideBar.filterTxt}</span>`
+      sidBar.appendChild(sideBarTop)
+      const main = document.querySelector(".mainTop-padding")
+      const topMain = document.createElement("div")
+      topMain.className = "top-content"
+      main.appendChild(topMain);
+      data.main.mainHeader.headerTop.forEach(item => {
+        const topItem = document.createElement("div")
+        topItem.className = "top-item"
+        topItem.innerHTML = `<a>${item.text}</a>
+         <img src="${item.topIcon}">`
+        topMain.appendChild(topItem)
       })
-
+      const topMiddle=document.createElement("div")
+      topMiddle.className="top-middle"
+      topMiddle.innerHTML=`<span>${data.main.mainHeader.mainHead}</span>`
+      main.appendChild(topMiddle)
+    
+      const headBtm=document.createElement("div")
+      headBtm.className="head-btm"
+      main.appendChild(headBtm)
+      const headUl=document.createElement("ul")
+      headBtm.appendChild(headUl)
+       data.main.mainHeader.headFilters.forEach(item=>{
+        const ulItem=document.createElement("li")
+        ulItem.innerText=item
+      headUl.appendChild(ulItem)
+       })
+ const cardPadding=document.querySelector(".card")
+       const card=document.createElement("div")
+        card.className="card"
+        cardPadding.appendChild(card)
     })
 })
